53142a7857be5ecdab85586d1cac88e668681ac9
Artur Chilingaryan <45322486+chartur@users.noreply.github.com>
2021-10-26 21:48:01 +0400

    web rtc api for electron

--- app.js
+++ app.js
@@ -1,56 +1,129 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-
 const events = {
-  CREATE_OR_JOIN_ROOM: 'create or emit room',
-  CREATED: 'created',
+  INIT: 'init',
+  CREATED: 'create',
   JOINED: 'joined',
-  ROOM_FULLED: 'full',
-  READY_TO_CONNECT: 'ready',
-  ON_CANDIDATE: 'candidate',
-  ON_OFFER: 'offer',
-  ON_ANSWERED:  'answer'
+  OFFER: 'offer',
+  ANSWER: 'answer',
+  ICE_CANDIDATES: 'ice-candidates',
+  JOINED_NEW: 'joined-new-user'
+};
+const ICE_config = {
+  'iceServer': [
+    {'urls': 'stun:stun.services.mozilla.com'},
+    {'urls': 'stun:stun.l.google.com:19302'}
+  ]
+};
+const videoCandidates = {
+  video: true,
+  audio: true //should be true
 };
 
+let socket;
+let localStream;
 
-app.use(express.static('public'));
-const port = process.env.PORT || 3000;
+const conversation = document.getElementById('conversation');
 
-io.on('connection', (socket) => {
-  socket.on(events.CREATE_OR_JOIN_ROOM, room => {
-    const myRoom = io.sockets.adapter.rooms.get(room) || new Map();
-    const numOfClients = myRoom.size;
-    console.log(myRoom);
-    if(numOfClients === 0) {
-      socket.join(room);
-      socket.emit(events.CREATED);
-    } else if (numOfClients === 1) {
-      socket.join(room);
-      socket.emit(events.JOINED, room);
-    } else {
-      socket.emit(events.ROOM_FULLED, room);
-    }
+window.onload = async () => {
+  const url = new URL(window.location.href);
+  const id = url.searchParams.get('id');
+
+  if(!id) {
+    window.location.href = '/';
+  }
+
+  await initMyVideo();
+
+  socket = io('http://localhost:3001', {
+    data: {id},
+    transports: [
+      'websocket'
+    ]
   });
 
-  socket.on(events.READY_TO_CONNECT, room => {
-    socket.broadcast.to(room).emit(events.READY_TO_CONNECT)
+  socket.on(events.JOINED, createReceiverPeerConnection);
+  socket.on(events.CREATED, createCallerPeerConnection);
+  socket.emit(events.INIT, id);
+};
+
+async function initMyVideo() {
+  const myVideoTag = document.getElementById('my-video');
+  localStream = await navigator.mediaDevices.getUserMedia(videoCandidates);
+  myVideoTag.srcObject = localStream;
+}
+
+async function createCallerPeerConnection() {
+  const peerConnection = new RTCPeerConnection(ICE_config);
+  peerConnection.onicecandidate = addIceCandidatesCallback;
+  peerConnection.ontrack = addNewStream;
+
+  for (let track of localStream.getTracks()) {
+    peerConnection.addTrack(track, localStream);
+  }
+
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  socket.on(events.ANSWER, (answer) => {
+    console.log('on_answer');
+    const remoteDesc = new RTCSessionDescription(answer);
+    peerConnection.setRemoteDescription(remoteDesc);
+  });
+  socket.on(events.ICE_CANDIDATES, (data) => {
+    console.log('on_ice_candidate');
+    peerConnection.addIceCandidate(data)
   });
 
-  socket.on(events.ON_CANDIDATE, event => {
-    socket.broadcast.to(event.room).emit(events.ON_CANDIDATE, event)
-  });
-
-  socket.on(events.ON_ANSWERED, event => {
-    socket.broadcast.to(event.room).emit(events.ON_ANSWERED, event)
-  });
-
-  socket.on(events.ON_OFFER, event => {
-    socket.broadcast.to(event.room).emit(events.ON_OFFER, event)
+  socket.on(events.JOINED_NEW, async () => {
+    const offer = await peerConnection.createOffer();
+    socket.emit(events.OFFER, offer)
   })
-});
-server.listen(port, () => {
-  console.log('server is listening on port ' + port)
-});
+}
+
+async function createReceiverPeerConnection() {
+  socket.on(events.OFFER, async (offer) => {
+    const peerConnection = new RTCPeerConnection(ICE_config);
+    peerConnection.onicecandidate = addIceCandidatesCallback;
+    peerConnection.ontrack = addNewStream;
+
+    for (let track of localStream.getTracks()) {
+      peerConnection.addTrack(track, localStream);
+    }
+
+
+    console.log(offer);
+    const remoteDesc = new RTCSessionDescription(offer);
+    await peerConnection.setRemoteDescription(remoteDesc);
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.emit(events.ANSWER, answer);
+
+    socket.on(events.ICE_CANDIDATES, (data) => {
+      console.log('on_ice_candidate');
+      peerConnection.addIceCandidate(data)
+    })
+  });
+}
+
+function addIceCandidatesCallback(event) {
+  if(!event.candidate) {
+    return;
+  }
+  socket.emit(events.ICE_CANDIDATES, event.candidate)
+}
+
+function addNewStream(trackEvent) {
+  const [stream] = trackEvent.streams;
+  let video = document.getElementById(stream.id);
+  if(!video) {
+    const div = document.createElement('div');
+    video = document.createElement('video');
+    video.id = stream.id;
+    video.autoplay = true;
+    video.srcObject = stream;
+    div.className = 'grid-item';
+    div.append(video);
+    conversation.append(div);
+    return;
+  }
+
+}
+
